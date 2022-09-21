d24efb8c32506c9fd1f7baa73e1cc041b1b91e6c
olga <bend_sinister@mail.ru>
2022-09-21 18:07:02 +0300

    MASHA improvements

--- Examples005/Program.cs
+++ Examples005/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter User Name");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("HURRA,THIS MASHA!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
+
