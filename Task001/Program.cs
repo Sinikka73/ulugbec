17e2cfc62d50c6f0a41c5ef165ae27acb820e9d0
olga <bend_sinister@mail.ru>
2022-09-23 22:11:26 +0300

    Добавил комментарий в конце

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите значение");
+int usernumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(usernumber);
+int result = usernumber * usernumber;
+Console.WriteLine($"Квадрат числа {usernumber} равен {result}"); //Находим квадрат числа
+
+
