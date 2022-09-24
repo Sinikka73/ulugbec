38e21d232afdddee9eeafc188c9234de0430d46a
olga <bend_sinister@mail.ru>
2022-09-24 19:06:36 +0300

    Добавили задачу 8

---
+++ Task008/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите любое натуральное число");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while(count < num)
+    {
+        if(count % 2 == 0)
+        Console.Write($"{count}, ");
+    count++;
+    }
