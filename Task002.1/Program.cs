cfa432ec2da54e84b213422a1e59857950c2df3e
olga <bend_sinister@mail.ru>
2022-09-24 16:50:43 +0300

    Добавили задачу про максимальное число

---
+++ Task002.1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 == number2) Console.WriteLine("Числа равны");
+else if(number1 > number2)Console.WriteLine($"Число {number1} максимальное");
+else Console.WriteLine($"Число {number2} максимальное");
+
