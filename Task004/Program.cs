176b8a22237e05b14d1774953656cf587e3f9323
olga <bend_sinister@mail.ru>
2022-09-24 13:06:28 +0300

    Добавили перевод вывод в ряд с - до +

---
+++ Task004/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите любое натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = -number;
+while(count < number)
+    {
+    Console.Write($"{count}, ");
+    count++;
+    }
+Console.Write($"{count}.");
+
+
