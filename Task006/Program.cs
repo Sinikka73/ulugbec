201085bd0624f5824f0ffb36c5e87de56763f8b2
olga <bend_sinister@mail.ru>
2022-09-24 18:38:20 +0300

    Добавили задачу чёт - нечёт

---
+++ Task006/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = num % 2;
+
+if(result == 0) Console.WriteLine($"Число {num} чётное");
+else Console.WriteLine($"Число {num} нечётное");
