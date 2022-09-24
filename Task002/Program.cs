a2c17607faf47bdf0d3b057c5682f4773c5fc99e
olga <bend_sinister@mail.ru>
2022-09-24 12:09:49 +0300

    Добавили задачу о квадрате

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите другое число для сравнения");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int result = number2 * number2;
+    
+if(result == number1)
+ {
+
+            Console.WriteLine($" Число {number1} является квадратом числа {number2}");
+
+ }
+
+else
+        {
+            Console.WriteLine($" Число {number1} не является квадратом числа {number2}");
+        }
