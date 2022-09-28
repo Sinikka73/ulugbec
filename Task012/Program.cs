d74a6906ce4ff64c1b83d333efc627f9514be433
olga <bend_sinister@mail.ru>
2022-09-28 20:39:00 +0300

    Добавили комментарии

--- Task012/Program.cs
+++ Task012/Program.cs
@@ -1,13 +1,13 @@
-﻿for(int i = 0; i < 10; i++)
+﻿for(int i = 0; i < 10; i++)//Даём возможность 10 раз пропустить код
 {
 Console.WriteLine("Введите первое число");
-int num1 = Convert.ToInt32(Console.ReadLine());
+int num1 = Convert.ToInt32(Console.ReadLine());//Перевод строки в формат числа
 Console.WriteLine("ВВедите второе число");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
 if(num1 > num2)
     {
-     int result = num1 % num2;
+     int result = num1 % num2;//Поиск остатка при делении
         if(result == 0)
             {
                 Console.WriteLine($"Число{num2} кратно числу {num1}.");
