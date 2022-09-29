774d44ff6470cb855b58d177bc9e513b9f4c83d6
olga <bend_sinister@mail.ru>
2022-09-29 13:00:28 +0300

    Задача  скратностью на 7 и 23

---
+++ Task013/Program.cs
@@ -0,0 +1,14 @@
+﻿int getAnsewerDivid7And23Simult(int number)//Задаём функцмю делимого числа и её параметры
+    {
+        int result = number % 7 + number % 23;
+        return result;    
+    }
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());//Перевод строки в формат числа
+int resul = getAnsewerDivid7And23Simult(num);
+    if(resul == 0)
+            {    
+                Console.WriteLine("ДА");       
+            }
+    else Console.WriteLine("НЕТ");
+
