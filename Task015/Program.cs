195a992e3644c4f6a21573dc36474c9bf916fc07
olga <bend_sinister@mail.ru>
2022-09-29 16:58:28 +0300

    Задача 15 выходные дни

---
+++ Task015/Program.cs
@@ -0,0 +1,12 @@
+﻿
+void getWeekend(int number)
+{
+    if(number == 6 || number == 7) Console.WriteLine("ДА");
+    else Console.WriteLine("НЕТ");
+}
+
+Console.WriteLine("Введите порядковый номер дня недели");
+int num = Convert.ToInt32(Console.ReadLine());//Перевод строки в формат числа
+getWeekend(num);
+
+
