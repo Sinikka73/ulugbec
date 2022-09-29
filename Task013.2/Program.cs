ad5980991525c3d2c23a2e0383b73f686e032818
olga <bend_sinister@mail.ru>
2022-09-29 16:23:45 +0300

    Задача о третьем числе слева

---
+++ Task013.2/Program.cs
@@ -0,0 +1,22 @@
+﻿int getDigit3FromNumber(int number)//Задаём параметры функции
+{
+    {
+        while(number > 999)
+        number = number / 10;    
+    }
+    int digit3 = number % 10;
+    return digit3;
+}   
+
+Console.WriteLine("Введите натуральное число");
+int num = Convert.ToInt32(Console.ReadLine());//Перевод строки в формат числа
+
+if(num > 99)
+    {
+         int result = getDigit3FromNumber(num);  
+         Console.WriteLine(result);
+    }
+else
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
