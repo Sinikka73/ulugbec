abd3df01fda2b77244bd65cf46b81ab2fcf3c6b2
olga <bend_sinister@mail.ru>
2022-10-02 19:47:38 +0300

    Lection 3

--- Examples012/Program.cs
+++ Examples012/Program.cs
@@ -70,9 +70,118 @@ string Metod4(int count, string c)
     return result;
 }
 
-string res = Metod4(100, "Trifle");
+string res = Metod4(10, "Trifle");
 Console.WriteLine(res);
 
+
+
+string Metod5(int count, string c)
+{
+    string result = String.Empty;
+    for(int i = 0; i < count; i++ )
+    {
+        result = result + c;
+    }
+    return result;
+}
+
+string resul = Metod5(10, "Tur");
+Console.WriteLine(resul);
+
+
+
+
+
+
+
+
+
+
+for(int i = 2; i<= 10; i++)
+    {
+        for(int j = 2; j <= 10; j++)
+            {
+                Console.WriteLine($"{i} X {j} = {i * j}");
+            }
+            Console.WriteLine();
+    }
+
+
+// Дан текст. В тексте нужно все пробелы заменить черточками, 
+// // // маленькие буквы “к” заменить большими “К”,
+// // // a большие “С” заменить маленькими “с”.
+// // // Ясна ли задача?
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//             01234
+// s [3]   // r
+
+string Replace(string text, char oldValue, char newValue)
+    {
+        string result = String.Empty;
+
+        int length = text.Length;
+        for(int i = 0; i < length; i++)
+            {
+                if(text[i] == oldValue) result = result + $"{newValue}";
+                else result = result + $"{text[i]}";                            
+            }      
+        return result;
+    }
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+
+Console.WriteLine();
+newText = Replace(newText, 'с', 'С');
+Console.WriteLine(newText);
+
+
+
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[]array)
+{
+    int count = array.Length;
+
+    for(int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}");
+
+    }
+    Console.WriteLine();
+}
+PrintArray(arr);
+
+void SelectionSort(int[] array)
+{
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        
+         for(int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+        //
+        //
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
 
 
 
