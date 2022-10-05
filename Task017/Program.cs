a943197878da5ca15375a2f0cc28521dab02fdae
olga <bend_sinister@mail.ru>
2022-10-05 17:38:02 +0300

    Функция нахождения расстояния между точками

---
+++ Task017/Program.cs
@@ -0,0 +1,65 @@
+﻿//  17. Напишите программу, которая принимает на вход координаты 
+  //  точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти 
+    //плоскости, в которой находится эта точка.                 
+
+int getQuaterFromCoordinate(int coordinateX, int coordinateY)
+    {
+        int result = 0;
+        if(coordinateX > 0 && coordinateY > 0)
+            {
+                result = 1;
+            } 
+        else if(coordinateX < 0 && coordinateY > 0)
+            {
+                result = 2;
+            }
+         else if(coordinateX < 0 && coordinateY < 0)
+            {
+                result = 3;
+            }   
+        else if(coordinateX > 0 && coordinateY < 0)
+        {
+            result = 4;
+        }    
+        return result;
+    }
+
+int getQuaterFromCoordinateFromUser()
+    {
+        int result = 0;
+        while(result == 0)
+        {
+            string userLine = Console.ReadLine();
+            int.TryParse(userLine, out result);
+            if(result == 0)
+                {
+                   Console.WriteLine($"Введите целое число.Вы ввели [{userLine}]");
+ 
+                }
+        }
+    
+    return result;
+    }
+int userCoordinateX = 0;
+int userCoordinateY = 0;
+Console.WriteLine("Введите координаты");
+
+Console.Write("Координата X:   ");
+userCoordinateX = getQuaterFromCoordinateFromUser();
+
+Console.Write("Координата Y:   ");
+userCoordinateY = getQuaterFromCoordinateFromUser();
+
+int resul = getQuaterFromCoordinate( userCoordinateX, userCoordinateY);
+    if(resul > 0)
+    {
+    Console.WriteLine($"Точка с координатами [{userCoordinateX}:{userCoordinateY}] находится в [{resul}] четверти");
+    }
+    else 
+    {
+      Console.WriteLine($"Одна из координат является нулевой");  
+    }
+    
+
+
+
---
+++ Task019/Program.cs
@@ -0,0 +1,50 @@
+﻿int getQuaterFromCoordinateFromUser()//Упреждение неправильного введения координат (введите целое число)
+    {
+        int resu = 0;
+        while(resu == 0)
+        {
+            string userLine = Console.ReadLine();
+            int.TryParse(userLine, out resu);
+            if(resu == 0 && userLine != "0")
+                {
+                   Console.WriteLine($"Введите целое число.Вы ввели [{userLine}]");
+ 
+                }
+            else
+                {
+                    break;
+                }    
+        }
+    
+    return resu;
+    }
+
+int X1 = 0;
+int Y1 = 0;
+int X2 = 0;
+int Y2 = 0;
+Console.WriteLine("Введите координаты точек");
+
+Console.Write("Координата X первой точки:   ");
+X1 = getQuaterFromCoordinateFromUser();
+
+Console.Write("Координата Y первой точки:   ");
+Y1 = getQuaterFromCoordinateFromUser();
+
+Console.Write("Координата X второй точки:   ");
+X2 = getQuaterFromCoordinateFromUser();
+
+Console.Write("Координата Y второй точки:   ");
+Y2 = getQuaterFromCoordinateFromUser();
+
+
+double getDistance(int x1, int x2, int y1, int y2) //функция нахождения расстояния
+{
+    double resul = 0;
+    resul = Math.Sqrt(Math.Pow((x2-x1),2) + Math.Pow((y2-y1),2));
+    return resul;
+}
+
+double result = getDistance(X1, X2, Y1, Y2);
+
+Console.WriteLine($"Точки с координатами [{X1}:{Y1}] и [{X2}:{Y2}] находятся на расстоянии {result} друг от друга.");
